f9c5b6745d6ab3aa5a19b0e5e533f34c09ae5321
miltonsousa <miltonsousa@live.com>
2019-09-28 00:48:39 -0300

    Tipos de Teste

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprinmentar(self):
-        return f'Ola {id(self)}'
+        return f'Ola, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_sonic():
@@ -17,10 +17,20 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprinmentar(self):
+        cumprimentar_da_classe=super().cumprinmentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+
+class Mutante(Pessoa):
+    olhos = 3
+
+
 
 if __name__ == '__main__':
-     costa = Pessoa(nome='Costa')
-     daniel = Pessoa(costa, nome='Daniel')
+     costa = Mutante(nome='Costa')
+     daniel = Homem(costa, nome='Daniel')
      print(Pessoa.cumprinmentar(daniel))
      print(id(daniel))
      print(daniel.cumprinmentar())
@@ -34,12 +44,23 @@ if __name__ == '__main__':
      del daniel.olhos
      print(daniel.__dict__)
      print(costa.__dict__)
-     Pessoa.olhos = 3
      print(Pessoa.olhos)
      print(daniel.olhos)
      print(costa.olhos)
      print(id(Pessoa.olhos)), id(daniel.olhos), id(costa.olhos)
      print(Pessoa.metodo_sonic(), daniel.metodo_sonic())
      print(Pessoa.nome_e_atributos_de_classe(), daniel.nome_e_atributos_de_classe())
+     pessoa=Pessoa('Anonimo')
+     print(isinstance(pessoa, Pessoa))
+     print(isinstance(pessoa, Homem))
+     print(isinstance(costa, Pessoa))
+     print(isinstance(costa, Homem))
+     print(costa.olhos)
+     print(costa.cumprinmentar())
+     print(daniel.cumprinmentar())
+
+
+
+
 
 
